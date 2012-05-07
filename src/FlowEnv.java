5f8d32e640a02f4bec15a3f1fd55a03d453d7116
Giry Geoffrey <giry.geoffrey@gmail.com>
2012-05-07 15:24:38 +0200

    change clone() method to  new constructor

--- src/FlowEnv.java
+++ src/FlowEnv.java
@@ -6,20 +6,23 @@ import com.google.dart.compiler.resolver.VariableElement;
 
 public class FlowEnv {
   private final HashMap<VariableElement, Type> variableTypeMap = new HashMap<>();
+  private final FlowEnv parent;
 
+  public FlowEnv(/*maybenull*/FlowEnv parent) {
+    this.parent = parent;
+  }
+  
   public Type getType(VariableElement variable) {
-    return variableTypeMap.get(variable);
+    Type type =  variableTypeMap.get(variable);
+    if (type == null) {
+      if (parent != null) {
+        return parent.getType(variable);
+      }
+    }
+    return type;
   }
 
   public void register(VariableElement variable, Type type) {
-    System.out.println(variable + ", " + type);
     variableTypeMap.put(variable, type);
-  }
-
-  @Override
-  public FlowEnv clone() {
-    FlowEnv flowEnv = new FlowEnv();
-    flowEnv.variableTypeMap.putAll(variableTypeMap);
-    return flowEnv;
   }
 }
