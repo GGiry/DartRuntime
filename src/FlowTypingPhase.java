5a29863833e420e5b9543d57cc6a91641167b591
forax <forax@univ-mlv.fr>
2012-05-11 21:11:12 +0200

    Fix, FlowTypingPhase doesn't compile

--- src/FlowTypingPhase.java
+++ src/FlowTypingPhase.java
@@ -176,7 +176,6 @@ public class FlowTypingPhase implements DartCompilationPhase {
     @Override
     public Type visitUnqualifiedInvocation(DartUnqualifiedInvocation node, FlowEnv parameter) {
       flowTyping(node.getTarget().getElement().getNode(), typeRepository);
-      node.get
       return accept(node.getTarget(), parameter);
       //return asType(false, node.getTarget().getType());
     }
