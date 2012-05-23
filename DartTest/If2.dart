19bdfab34e44466f9005dc9b10e7a8b0ac5eebd0
Giry Geoffrey <giry.geoffrey@gmail.com>
2012-05-23 13:17:17 +0200

    minor changes

--- DartTest/If2.dart
+++ DartTest/If2.dart
@@ -1,33 +1,35 @@
 class A {
   int foo;
+}
 
-  void main() {
-    var a = foo;
-    
-    var b;
-    var c;
-    var d;
-    
-    var a1;
-    var a2;
-    var a3;
-    var a4;
-    
-    if (a == 13) {
-    // a == 13
-      a1 = a;
-      if (a == 3) {
-        // on ne rentre pas.
-        b = 10;
-        a2 = a;
-      }
-      c = 1.2;
-    } else if (a != 15) {
-      // a == int[-inf; 14)] U int[16; +inf]
-      a3 = a; 
-    } else {
-      a4 = a;
-      d = true;
+void main() {
+  var aa = new A();
+
+  var a = aa.foo;
+  
+  var b;
+  var c;
+  var d;
+  
+  var a1;
+  var a2;
+  var a3;
+  var a4;
+  
+  if (a == 13) {
+  // a == 13
+    a1 = a;
+    if (a == 3) {
+      // on ne rentre pas.
+      b = 10;
+      a2 = a;
     }
+    c = 1.2;
+  } else if (a != 15) {
+    // a == int[-inf; 14)] U int[16; +inf]
+    a3 = a; 
+  } else {
+    a4 = a;
+    d = true;
   }
 }
