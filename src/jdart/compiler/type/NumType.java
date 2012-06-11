0d0eedf0fd2b130a053a4336ed836713b083502d
Giry Geoffrey <giry.geoffrey@gmail.com>
2012-06-11 11:55:38 +0200

    Add documentation on NumType.

--- src/jdart/compiler/type/NumType.java
+++ src/jdart/compiler/type/NumType.java
@@ -1,9 +1,27 @@
 package jdart.compiler.type;
 
 public interface NumType extends Type {
+  /**
+   * Returns the result of the operation add on this type and the specified type.
+   * 
+   * @param other Type to add.
+   * @return The result or null if other is not computable.
+   */
   Type add(Type other);
 
+  /**
+   * Returns the result of the operation subtract on this type and the specified type.
+   * 
+   * @param other Type to subtract.
+   * @return The result or null if other is not computable.
+   */
   Type sub(Type other);
 
+  /**
+   * Returns the result of the operation modulo on this type and the specified type.
+   * 
+   * @param other Type to compute.
+   * @return The result or null if other is not computable.
+   */
   Type mod(Type other);
 }
